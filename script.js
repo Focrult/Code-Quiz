4c96f84721a8271582ce3b6f13f07ca2aa9dea76
Focrult <focrult@gmail.com>
2022-12-04 14:46:54 +0800

    Starting Challenge #4

---
+++ script.js
@@ -0,0 +1,16 @@
+const Displaytime = document.querySelector("#Displaytime");
+const startQuiz = document.querySelector("#startQuiz");
+
+let startTime = 75;
+
+
+
+
+startQuiz.addEventListener("click", () => {
+    if(){
+
+    }
+
+});
+
+function
