669f2418188b0eda54ff7fd65447308569941c04
Focrult <focrult@gmail.com>
2022-12-05 09:38:38 +0800

    Made changes to each file, adding a better layout

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const Displaytime = document.querySelector("#Displaytime");
-const startQuiz = document.querySelector("#startQuiz");
-const resetBtn = documeent.querySelector("#redo");
-let startTime = 75;
-let secs = 75;
+const Displaytime = document.getElementById("Displaytime");
+const startBtn = document.getElementById("startBtn");
+const resetBtn = documeent.getElementById("redo");
+const QuestionContainer = document.getElementById("QuestionContainer"); //shows questions after start
+
+startBtn.addEventListener("click", start)
 
 
-
-startQuiz.addEventListener("click", () => {
-    if(){
-
-    }
-
-});
-
-
-
-
-function time(){
-    secs = Math.floor((1000 % 60));
+function start(event){
+startBtn.classList.add('hide');
 }
+
+
+
+
+//timer function countdown
+function timer(){
+    var timer = 75;
+    var timerinterval = setInterval(function (){
+        if(timer > 1){
+            Displaytime.textContent = timer;
+            timer--;
+        }else {
+            Displaytime.textContent = '';
+
+            clearInterval(timetinterval);
+        }
+    }, 1000);
+}
+
